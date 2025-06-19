6a28bdcf63420c3b445c2c42e7cea1ae7177eed5
Absterrg0 <parvj5212@gmail.com>
2025-06-19 19:12:01 +0530

    im dumb

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,8 +5,8 @@ import Google from "next-auth/providers/google";
 export const {handlers,auth,signIn,signOut} = NextAuth({
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID || "182739669714-df4sf521et27q7bi5d6dunf45i2g2v13.apps.googleusercontent.com",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "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",
+      clientId: process.env.GOOGLE_CLIENT_ID ,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ,
       profile(profile) {
         return {
           id: profile.sub,
